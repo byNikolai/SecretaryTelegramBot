221b6b9810d6d28c6809fdb8761c3891079d90f8
By_Nickolay <by.nickolay@gmail.com>
2023-10-12 19:48:22 +0300

    Initial commit

---
+++ src/main/java/com/byNickolay/SecretaryTelegramBot/SecretaryTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.byNickolay.SecretaryTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretaryTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretaryTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byNickolay/SecretaryTelegramBot/SecretaryTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byNickolay.SecretaryTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretaryTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
